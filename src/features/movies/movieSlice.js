6c9f65d29d9d1ee3602624fbdd6534e51c9ce4fa
GUchechi <godswilluchendu7@gmail.com>
2022-12-05 15:41:11 +0100

    redux-movie

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/MovieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await movieApi
+      .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log("Err :", err);
+      });
+    return response.data;
+  }
+);
 
 const initialState = {
-movies: {},
+  movies: {},
 };
 
 const movieSlice = createSlice({
@@ -10,6 +25,18 @@ const movieSlice = createSlice({
   reducers: {
     addMovies: (state, { payload }) => {
       state.movies = payload;
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully");
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected!!!");
     },
   },
 });
